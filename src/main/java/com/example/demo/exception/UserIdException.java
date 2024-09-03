97eaa58ca1529b0d5d3281a472a8de5a4ef6428b
Shaikkaif <kaifkaishrockz@gmail.com>
2024-09-03 10:59:16 +0530

    Added

---
+++ src/main/java/com/example/demo/exception/UserIdException.java
@@ -0,0 +1,14 @@
+package com.example.demo.exception;
+
+public class UserIdException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UserIdException(String msg) {
+		super(msg);
+	}
+
+}
